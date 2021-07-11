e9029e984e61f89b5cbee483b59e656f34ae5bef
Carolwanzuu <carolkyalo01@gmail.com>
2021-07-11 12:17:18 +0300

    adding url paths

--- photos/urls.py
+++ photos/urls.py
@@ -4,4 +4,7 @@ from . import views
 urlpatterns=[
     url('^$',views.welcome,name = 'home'),
     url('about/', views.about, name = 'about'),
+    url('add_post/', views.about, name = 'add_post'),
+    url('<int:pk>/', views.about, name = 'post_detail'),
+    url('<int:pk>/', views.about, name = 'likes'),
 ]
